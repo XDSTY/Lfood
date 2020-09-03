419bc8c7e624759c69e894c246c9bd18ab56e55b
zhangfuhua <zhangfuhua@zuche.com>
2020-09-03 17:27:11 +0800

    添加docker

---
+++ orderservice/docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+project_name=algorderservice
+repository=47.107.99.226:8081
+hostname=$HOSTNAME
+
+# 停止对应的容器
+docker container  stop `docker container ls | grep "${project_name}" | awk '{print $1}'`
+# 删除停止的容器
+# docker container  rm `docker ps -a | grep Exited | awk '{print $1}'`
+docker rm $(docker ps -a -q)
+# 删除对应的镜像
+docker rmi `docker image ls | grep "${project_name}" | awk '{print $1}'`
+
+docker pull ${repository}/${project_name}
+docker run -d -v /etc/hosts:/etc/hosts --hostname ${hostname} -v /home/workspace/${project_name}/log:/log -p 8086:8086 -p 20882:20882  ${repository}/${project_name}
